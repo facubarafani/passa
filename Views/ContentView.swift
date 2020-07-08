85622ca4509b8e2c402b1d75e1efcf1e4b1a19a3
Facundo Barafani <facu1910@gmail.com>
2020-07-08 14:46:23 -0300

    Change event vertical for horizontal scroll and add animation when scrolling

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -14,32 +14,40 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView{
-            ScrollView(.vertical,showsIndicators: false) {
-                VStack (alignment: .center, spacing: 20){
-                    ForEach(eventData.events) { item in
-                    EventCardView(event: item)
+            
+            VStack {
+                Text("Próximos Eventos")
+                    .font(Font.title.weight(.bold))
+                ScrollView(.horizontal,showsIndicators: false) {
+                    HStack (alignment: .center, spacing: 20){
+                        ForEach(eventData.events) { item in
+                            GeometryReader { geometry in
+                                EventCardView(event: item)
+                                    .rotation3DEffect(Angle(degrees: (Double(geometry.frame(in: .global).minX)-15) / -30), axis: (x: 0, y: 10, z: 0))
+                            }
+                        }
                     }
-                }
-                .frame(maxWidth:640)
-                .padding(.horizontal)
+                    .frame(width: 640, height: 480)
+                    .padding(15)
                     .onAppear {
                         self.eventData.getEvents()
+                    }
+                    .navigationBarTitle("Passa",displayMode: .inline)
+                    .navigationBarItems(
+                        leading:
+                        NavigationLink(destination: CameraView()) {
+                            Image(systemName: "camera")
                         }
-                        .navigationBarTitle("Passa",displayMode: .inline)
-                        .navigationBarItems(
-                            leading:
-                                NavigationLink(destination: CameraView()) {
-                                    Image(systemName: "camera")
-                            }
-                            ,trailing:
-                            NavigationLink(destination: EventsView()) {
-                                Image(systemName: "list.dash")
-                            }
+                        ,trailing:
+                        NavigationLink(destination: EventsView()) {
+                            Image(systemName: "list.dash")
+                        }
                     )
+                }
             }
         }
-        }
     }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
